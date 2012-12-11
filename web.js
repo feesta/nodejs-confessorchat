12f269a8401d4450432e38e08ca0daa336883900
feesta <feesta@gmail.com>
2012-12-11 00:45:28 +0000

    Cleaned up back and forth for Android device

--- web.js
+++ web.js
@@ -1,28 +1,27 @@
 var express = require("express"),
-    http = require("http");
+    http = require("http"),
+    app = express(),
+    server = http.createServer(app),
 
-var app = express();
-var server = http.createServer(app);
-//var io = require("socket.io").listen(server);
+    redis = require("redis"),
+    db = redis.createClient(),
 
-var redis = require("redis");
-var db = redis.createClient();
+    gcm = require('node-gcm'),
+    sender = new gcm.Sender('AIzaSyArgLI8MNgs5jovl3aIPv-uQH9jcAEyl9k'),
 
-attempts_limit = 8;
+    attempts_limit = 8,
+    //        0             1            2           3
+    STATE = ["confession", "confessed", "forgiven", "forgave"],
 
+    port = process.env.PORT || 80;
 
-var gcm = require('node-gcm');
-var sender = new gcm.Sender('AIzaSyArgLI8MNgs5jovl3aIPv-uQH9jcAEyl9k');
-var registrationIds = [];
-
-var port = process.env.PORT || 80;
 server.listen(port);
 
 app.get("/", function (req, res) {
     // nothing here?
     //res.sendfile(__dirname + "/index.html");
-    console.info("req", req.query);
-    res.send("hi");
+    console.info("'/': req", req.query);
+    res.send("confessorapp");
 });
 
 app.get("/check", function (req, res) {
@@ -40,30 +39,41 @@ app.get("/check", function (req, res) {
             console.info("user not added:", user);
         
         // check for messages and send response.
-        db.zrevrange("confessionqueue:" + user, "-10", "-1", function(error, data) {
-            if (error) result.send(JSON.stringify({status:"error", message:"error getting queue"}));
-            else {
-                console.info("queue:", data.join(", "));
-                if (data && data.length) {
-                    db.mget(data, function(error, data) {
-                        if (error) result.send(JSON.stringify({status:"error", message:"error getting queue"}));
-                        else {
-                            //result.send("data" + data);
-                            var json_result = {status:"success", data:[]};
-                            for (var i = 0; i < data.length; i++) {
-                                json_result.data.push(JSON.parse(data[i]));
-                            }
-                            result.send(JSON.stringify(json_result));
-                        }
-                    });
-                } else {
-                    result.send(JSON.stringify({status:"success", data:[]}));
-                }
-            }
-        });
+        check(result, user);
     });
 });
 
+function check(result, user) {
+    db.zrevrange("messagequeue:" + user, "-10", "-1", "WITHSCORES", function(error, data) {
+        if (error) result.send(JSON.stringify({status:"error", message:"error getting queue"}));
+        else {
+            console.info("message queue:", data.join(", "));
+            if (data && data.length) {
+                var dataList = [],
+                    valueList = [];
+                for (var i = 0; i < data.length; i = i + 2) {
+                    dataList.push(data[i]);
+                    valueList.push(data[i+1]);
+                }
+                db.mget(dataList, function(error, data) {
+                    if (error) result.send(JSON.stringify({status:"error", message:"error getting queue"}));
+                    else {
+                        //result.send("data" + data);
+                        var json_result = {status:"success", data:[]};
+                        for (var i = 0; i < data.length; i++) {
+                            var item = JSON.parse(data[i]);
+                            item['state'] = STATE[valueList[i].slice(-1)];
+                            json_result.data.push(item);
+                        }
+                        result.send(JSON.stringify(json_result));
+                    }
+                });
+            } else {
+                result.send(JSON.stringify({status:"success", data:[]}));
+            }
+        }
+    });
+}
 
 app.get("/confess", function (req, res) {
     console.info("/confess");
@@ -88,7 +98,11 @@ app.get("/confess", function (req, res) {
             console.log("received:", data, " from ", confessObj.user_id, "id", confessObj.id, "confessionhash:" + confessObj.id);
             db.zadd("confession_queue", Date.now(), "confession:" + confessObj.id);
             db.zadd("confessions", Date.now(), "confession:" + confessObj.id);
-            db.set("confessionhash:" + confessObj.id, JSON.stringify(confessObj));
+            db.set("confessionhash:" + confessObj.id, JSON.stringify({
+                    id:confessObj.id,
+                    message:confessObj.message,
+                    //user_id:confessObj.user_id,
+                    date:confessObj.date}));
             db.hmset("confession:" + confessObj.id,
                      "message", confessObj.message, 
                      "user_id", confessObj.user_id, 
@@ -98,12 +112,29 @@ app.get("/confess", function (req, res) {
                      "forgivers", "forgivers:confession:" + confessObj.id, 
                      "date", confessObj.date);
             db.sadd("forgivers:confession:" + confessObj.id, confessObj.user_id);
-            result.send(JSON.stringify({status:"success"}));
+            db.zadd("messagequeue:" + user, Date.now() + "0", "confessionhash:" + confessObj.id, function(err, data) {
+                check(result, user);
+            });
+            //result.send(JSON.stringify({status:"success"}));
         });
     });
 });
 
 
+app.get("/removeMessage", function (req, res) {
+    // update message
+    // queue forgiveness
+    if (needsParams(["user_id", "confess_id"], req.query, res)) return;
+    var regId = req.query.user_id;
+    var confess_id = req.query.confess_id;
+    var result = res;
+    
+    console.info("query:", req.query);
+    // remove from forgivers messagequeue
+    console.info("removing:", "messagequeue:user_" + regId, "confessionhash:" + confess_id);
+    db.zrem("messagequeue:user_" + regId, "confessionhash:" + confess_id);
+    check(result, "user_" + regId);
+});
 app.get("/forgive", function (req, res) {
     // update message
     // queue forgiveness
@@ -113,15 +144,30 @@ app.get("/forgive", function (req, res) {
     var forgiven = req.query.forgiven;
     var result = res;
     
-    res.send("{status:'success'}");
+    //res.send("{status:'success'}");
     console.info("query:", req.query);
-
-    if (forgiven) {
-    // remove confess_id from confession_queue
+    
+    if (forgiven == "true") {
+        // remove confess_id from confession_queue
         db.zrem("confession_queue", "confession:" + confess_id);
-    // set confess_id status to 'forgiven'
+        // TODO: Setting a new score reorders the forgiven message. use the old score but with a new status digit.
+        db.zadd("messagequeue:user_" + regId, Date.now() + "3", "confessionhash:" + confess_id);
+        // set confess_id status to 'forgiven'
         db.hset("confession:" + confess_id, "status", "forgiven");
+        // add to confessors messagequeue
+        db.hget("confession:" + confess_id, "user_id", function(err, data) {
+            console.info("add ", confess_id, "to user:", data);
+            db.zadd("messagequeue:" + data, Date.now() + "2", "confessionhash:" + confess_id);
+            // send forgiveness message to device...
+            db.hget("user:" + data, "regId", function(err, data) {
+                sendToAndroid(data, "You have been forgiven");
+            });
+        });
+        check(result, "user_" + regId);
     } else {
+        // remove from forgivers messagequeue
+        console.info("removing:", "messagequeue:user_" + regId, "confessionhash:" + confess_id);
+        db.zrem("messagequeue:user_" + regId, "confessionhash:" + confess_id);
     }
     
     
@@ -145,7 +191,7 @@ app.get("/processQueue", function (req, res) {
             db.hgetall(confess_id, function(err, confessObj) {
                 //console.info(JSON.stringify(confessObj));
                 var message = confessObj.message,
-                    user_id_raw = confessObj.user_id,
+                    //user_id_raw = confessObj.user_id,
                     confession_id = confessObj.confession_id,
                     status = confessObj.status,
                     attempts = confessObj.attempts,
@@ -167,7 +213,7 @@ app.get("/processQueue", function (req, res) {
                             function(confess_id, forgiver_id) {
                                 console.info("random user:", forgiver_id, confess_id);
                                 db.sadd("forgivers:" + confess_id, forgiver_id);
-                                db.zadd("confessionqueue:" + forgiver_id, Date.now(), confess_id.replace("confession:", "confessionhash:"));
+                                db.zadd("messagequeue:" + forgiver_id, Date.now() + "1", confess_id.replace("confession:", "confessionhash:"));
                                 sendToUser(confess_id, forgiver_id);
                             }
                         );
@@ -203,19 +249,19 @@ function needsParams(required, query, res) {
     }
     if (passed) return false;
     else {
-        res.send(JSON.stringify({status:"error", message:"error - missing message"}));
+        res.send(JSON.stringify({status:"error", message:"error - missing variable"}));
         return needs;
     }
 }
 
-function addUser(user_id, device, res) { return function (callback) {
+function addUser(regId, device, res) { return function (callback) {
     // check if in database
     // add other info like device and how to send info back.
-    var user = "user_" + user_id;
-    db.hmset(user,
+    var user = "user_" + regId;
+    db.hmset("user:" + user,
         "user_id", user,
         "device", device,
-        "regId", user_id,
+        "regId", regId,
         function(error, data) {
             db.sadd("users", user, function(error, added) {
                 callback(user, added);
@@ -257,7 +303,6 @@ function getRandomUser(confess_id, tries) { return function(callback) {
         });
     });
 }}
-
 
 
 
